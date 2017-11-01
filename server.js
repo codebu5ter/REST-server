1be2934e69f1daffda958a39aeb44031e4bcbc6f
Nikhil Nayak <iamnikhilnayak@outlook.com>
2017-11-01 17:30:43 +0530

    Add Push functionality

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+  {
+      "id":"232kAk",
+      "text":"Eggs"
+  },
+  {
+      "id":"d23iOk",
+      "text":"Milk"
+  },
+  {
+      "id":"cbv12A",
+      "text":"Bacon"
+  },
+  {
+      "id":"ifo98j",
+      "text":"Chicken"
+  },
+];
+
 app.get('/', function(request, response) {
-  response.send('My first API!');
+  response.send(ingredients);
+});
+
+app.post('/', function(request, response) {
+  var ingredient = request.body;
+  if (!ingredient || ingredient.text == "") {
+    response.status(500).send({error: "Your ingredient must have some text!"});
+  } else {
+    ingredients.push(ingredient);
+    response.status(200).send(ingredient);
+  }
 });
 
 app.get('/test', function(req, res) {
-  res.send('This is the test subdomain!')
-})
+  res.send('This is the test subdomain!');
+});
 
 app.listen(3000, function() {
   console.log("First API running on port 3000!");
